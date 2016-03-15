00c882135dc7afda18c0b3e7fd440c385d8b31f6
Jonathan Reeve <jon.reeve@gmail.com>
2016-03-15 11:08:50 -0400

    new analysis for x-of-xs

---
+++ x-of-xs.py
@@ -0,0 +1,49 @@
+import re
+import glob
+import os
+from termcolor import colored
+import click
+
+blacklist = ['it of its']
+
+def cleanText(text): 
+    text = re.sub('\n', ' ', text) # replace newlines with spaces
+    text = re.sub('\s+', ' ', text) # collapse whitespace
+    return text
+
+def findKJVism(text, filename, context=20): 
+    matches = re.finditer(r'(\b(.+?)\b\sof\s\2s)', text)
+    counter = 0
+    for match in matches: 
+        matchText = cleanText(match.group(0))
+        if matchText in blacklist: # ignore blacklisted matches
+            continue
+        filename = os.path.basename(filename)
+        contextBefore = text[match.span()[0] - context:match.span()[0]]
+        contextAfter = text[match.span()[1] : match.span()[1] + context ]  
+        out = filename + ": " + contextBefore + colored(matchText, 'red') + contextAfter
+        out = cleanText(out) # sanitize output again
+        counter += 1
+        print(out)
+    if counter > 0: 
+        print(counter, 'total matches found in ' + filename)
+    return counter
+
+
+@click.command()
+@click.argument('filenames', nargs=-1)
+@click.option('-l', '--logfile', default='x-of-xs-log.txt', help='The name of the log file to write to.')
+def cli(filenames, logfile):
+    for filename in filenames: 
+        text = open(filename).read().lower()
+        count = findKJVism(text, filename)
+        # Write to log
+        if count > 0: 
+            line = [filename, str(count)]  
+            line = ",".join(line) + '\n'
+            f = open(logfile, 'a')
+            f.write(line)
+            f.close()
+
+if __name__ == '__main__':
+    cli()
